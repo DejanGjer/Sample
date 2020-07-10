4ad8826d2a686c30cc16dbe598eea28300f3fb29
dejan <dejangjer@gmail.com>
2020-07-10 10:30:35 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Unesite dva broja" << "\n";
+    int a,b;
+    cin >> a >> b;
+    cout << "Zbir je: " << a + b;
+    return 0;
+}
